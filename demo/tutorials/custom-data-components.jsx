445f609c0bd2df3fdb919b4ec6d5562f73277ea6
Stefan Huynh <huynh.stefan@gmail.com>
2016-05-05 12:42:57 -0700

    Add initial graph with axes

--- demo/tutorials/custom-data-components.jsx
+++ demo/tutorials/custom-data-components.jsx
@@ -1,11 +1,57 @@
 import React from "react";
 import Radium from "radium";
 
+// VComponents
+import { VictoryAxis, VictoryLine } from "victory-chart";
+
 class CustomDataComponents extends React.Component {
+  getStyles() {
+    return {
+      parent: {
+        boxSizing: "border-box",
+        display: "block",
+        width: "100%",
+        height: "100%",
+        padding: 50
+      }
+    };
+  }
+
   render() {
+    const styles = this.getStyles();
+
     return (
       <div>
-        Hello, world!
+        <svg style={styles.parent} viewBox="0 0 500 300">
+          <VictoryAxis
+            standalone={false}
+            tickValues={[
+              "Halifax",
+              "Montreal",
+              "Quebec",
+              "Ottawa",
+              "Toronto",
+              "Calgary",
+              "Vancouver"
+            ]}
+            label="City"
+            style={{
+              grid: {stroke: "rgba(0, 0, 0, 0.1)"},
+              axis: {stroke: "none"},
+              ticks: {stroke: "none"}
+            }}
+          />
+          <VictoryAxis dependentAxis
+            standalone={false}
+            domain={[0, 350]}
+            label="cm"
+            style={{
+              grid: {stroke: "rgba(0, 0, 0, 0.1)"},
+              axis: {stroke: "none"},
+              ticks: {stroke: "none"}
+            }}
+          />
+        </svg>
       </div>
     );
   }
