774c6e26912f4a2343a0b56765b69d2fa693f5cc
ebrillhart <emma.brillhart@gmail.com>
2016-06-28 14:59:04 -0700

    adding basic bar aspects and interactivity to switch between data sets

--- src/components/candlestick-dashboard.js
+++ src/components/candlestick-dashboard.js
@@ -2,95 +2,106 @@ import React from "react";
 import Radium from "radium";
 
 // VComponents
-import { VictoryAxis, VictoryBar, VictoryStack, VictoryLabel } from "victory";
+import { random } from "lodash";
+import { VictoryAxis, VictoryBar, VictoryArea } from "victory";
+
+const dataSet = {
+  week: [
+    {x: "Week 1", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 2", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 3", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 4", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 5", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 6", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 7", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 8", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 9", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 10", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 11", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "Week 12", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)}
+  ],
+  day: [
+    {x: "6:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "7:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "8:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "9:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "10:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "11:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "12:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "1:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "2:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "3:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "4:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "5:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "6:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "4:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)}
+  ],
+  hour: [
+    {x: "6:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "7:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "8:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "9:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "10:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "11:00am", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "12:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "1:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "2:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "3:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "4:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "5:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)},
+    {x: "6:00pm", open: 20, close: 34, high: 80, low: 15, volume: random(0, 100)}
+  ]
+};
+
+const labels = {
+  hour: ["6:00am", "12:00pm", "6:00pm"],
+  week: ["Week 1", "Week 4", "Week 8", "Week 12"],
+  day: ["Day 1", "Day 7", "Day 14"]
+};
 
 class CandlestickDashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      period: "hour",
+      data: [],
+      labels: labels.hour
+    };
+
+    this.onToggleDay = this.onToggleDay.bind(this);
+    this.onToggleWeek = this.onToggleWeek.bind(this);
+    this.onToggleHour = this.onToggleHour.bind(this);
+  }
+
+  onToggleWeek() {
+    this.setState({period: "week", data: dataSet.week, labels: labels.week});
+  }
+
+  onToggleDay() {
+    this.setState({period: "day", data: dataSet.day, labels: labels.day});
+  }
+
+  onToggleHour() {
+    this.setState({period: "day", data: dataSet.hour, labels: labels.hour});
+  }
+
   render() {
     return (
-      <svg style={{width: "100%", height: "auto"}} viewBox="0 0 500 300">
-
-        <VictoryStack horizontal
-          /* setting a symmetric domain makes it much easier to center the axis */
-          domain={{x: [-60, 60]}}
-          /*
-            When not using two adjacent Victory components without a wrapper component
-            like VictoryChart or VictoryStack width, height and padding padding must be
-            set in top level component so that they match up with each other.
-          */
-          height={300}
-          width={500}
-          padding={50}
-          standalone={false}
-          style={{
-            data: {width: 20},
-            labels: {fontSize: 11}
-          }}
-        >
+      <div>
+        <button onClick={this.onToggleWeek}>1wk</button>
+        <button onClick={this.onToggleDay}>1d</button>
+        <button onClick={this.onToggleHour}>1hr</button>
+        <svg style={{width: "100%", height: "auto"}} viewBox="0 0 500 300">
           <VictoryBar
-            style={{data: {fill: "tomato"}, labels: {padding: -5}}}
-            data={[
-              {x: "Smartphone", y: 57},
-              {x: "Laptop", y: 36},
-              {x: "Televsion", y: 38},
-              {x: "Tablet", y: 38},
-              {x: "Fitness Monitor", y: 12},
-              {x: "Smartwatch", y: 12},
-              {x: "Surveillance Camera", y: 10},
-              {x: "Smart Thermostat", y: 9},
-              {x: "Personal Drones", y: 6}
-            ]}
-            x={"x"}
-            y={(data) => (-Math.abs(data.y))}
-            labels={(data) => (`${Math.abs(data.y)}%`)}
+            data={this.state.data}
+            y={"volume"}
           />
-          <VictoryBar
-            style={{data: {fill: "orange"}}}
-            data={[
-              {x: "Smartphone", y: 48},
-              {x: "Laptop", y: 30},
-              {x: "Television", y: 30},
-              {x: "Tablet", y: 29},
-              {x: "Fitness Monitor", y: 13},
-              {x: "Smartwatch", y: 13},
-              {x: "Surveillance Camera", y: 11},
-              {x: "Smart Thermostat", y: 9},
-              {x: "Personal Drones", y: 7}
-            ]}
-            labels={(data) => (`${Math.abs(data.y)}%`)}
+          <VictoryAxis
+            scale={"time"}
+            tickValues={this.state.labels}
           />
-        </VictoryStack>
-
-        <VictoryAxis dependentAxis
-          height={300}
-          width={500}
-          padding={50}
-          style={{
-            axis: {stroke: "transparent"},
-            ticks: {stroke: "transparent"},
-            tickLabels: {fontSize: 7, fill: "black"}
-          }}
-          /*
-            Use a custom tickLabelComponent with an absolutely positioned x value
-            to position your tick labels in the center of the chart. The correct
-            y values are still provided by VictoryAxis for each tick
-          */
-          tickLabelComponent={
-            <VictoryLabel x={250} textAnchor="middle" verticalAnchor="middle"/>
-          }
-          standalone={false}
-          tickValues={[
-            "Smartphone",
-            "Laptop",
-            "Television",
-            "Tablet",
-            "Fitness Monitor",
-            "Smartwatch",
-            "Surveillance Camera",
-            "Smart Thermostat",
-            "Personal Drones"
-          ]}
-        />
-      </svg>
+        </svg>
+      </div>
     );
   }
 }
